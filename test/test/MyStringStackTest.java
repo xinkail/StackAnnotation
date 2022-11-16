7d87c9d6032527492090356d3150435f2b0be090
xinkail <118232948+xinkail@users.noreply.github.com>
2022-11-16 14:41:56 +0800

    Update MyStringStackTest.java

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -32,8 +32,10 @@ public class MyStringStackTest extends TestCase {
 	@Test
 	public void testPush() {
 		MyStringStack stack = new MyStringStack();
-		stack.push (s1);
 		assertTrue (stack.isEmpty());
+		stack.push (s1);
+       		stack.push (s2);
+        	assertFalse (stack.isEmpty());
 	}
 
 	@Test
